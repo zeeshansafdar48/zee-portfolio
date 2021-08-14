eb69d9daadaac3ee7f78b52e07e6c63bf8da8c3c
Zeeshan Safdar <zeeshan7826@gmail.com>
2021-08-14 15:37:58 +0500

    remove storage logic

--- src/hooks/useTheme.tsx
+++ src/hooks/useTheme.tsx
@@ -1,17 +1,7 @@
 import { useState } from 'react';
-// import storage from 'local-storage-fallback';
 
 export default function useTheme() {
   const [colourTheme, setColourTheme] = useState({ mode: 'dark' });
-
-  // function getInitialTheme() {
-  //   const savedTheme = storage.getItem('colourTheme');
-  //   return savedTheme ? JSON.parse(savedTheme) : defaultTheme;
-  // }
-
-  // useEffect(() => {
-  //   storage.setItem('colourTheme', JSON.stringify(colourTheme));
-  // }, [colourTheme]);
 
   return {
     ...colourTheme,
